306ed4bbcd17e5b058f4b535f01bbfb2514ff2bc
Ayinla Abel <kraneabel@gmail.com>
2022-07-14 05:55:31 -0700

    monty.h updated

--- monty.h
+++ monty.h
@@ -9,22 +9,13 @@
 #include <ctype.h>
 
 /**
- *
- *  * struct stack_s - doubly linked list representation of a stack (or queue)
- *
- *   * @n: integer
- *
- *    * @prev: points to the previous element of the stack (or queue)
- *
- *     * @next: points to the next element of the stack (or queue)
- *
- *      *
- *
- *       * Description: doubly linked list node structure
- *
- *        * for stack, queues, LIFO, FIFO Holberton project
- *
- *         */
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
 
 typedef struct stack_s
 {
@@ -75,13 +66,9 @@ extern bus_t bus;
  *        */
 
 typedef struct instruction_s
-
 {
-
-		char *opcode;
-
-			void (*f)(stack_t **stack, unsigned int line_number);
-
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
